ac3ea29c76aa70706869e672e561c8508d7186ff
Muhindhar <muhindharsv27@gmail.com>
2026-06-15 22:20:07 +0530

    json

--- src/test/java/com/tests/GetUserTest.java
+++ src/test/java/com/tests/GetUserTest.java
@@ -7,23 +7,23 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class GetUserTest {
-  @Test
-  public void f() {
-	  
-	  Response response = RestAssured.given().when().get("https://jsonplaceholder.typicode.com/users");
-	  System.out.println("status code : "+response.getStatusCode());
-	  response.prettyPrint();
-	  Assert.assertEquals(response.getStatusCode(),200);
-	  
-	  String name = response.jsonPath().getString("name");
-	  System.out.println(name);
-	  Assert.assertEquals(name, "Leanne Graham");
-	  String uname = response.jsonPath().getString("username");
-	  System.out.println(uname);
-	  Assert.assertEquals(uname, "Bret");
-	  String email = response.jsonPath().getString("email");
-	  System.out.println(email);
-	  Assert.assertEquals(email, "Sincere@april.biz");
-	  
-  }
+
+	@Test
+	public void f() {
+
+		Response response = RestAssured.given().when().get("https://jsonplaceholder.typicode.com/users");
+
+		System.out.println("status code : " + response.getStatusCode());
+
+		Assert.assertEquals(response.getStatusCode(), 200);
+
+		String name = response.jsonPath().getString("[0].name");
+		Assert.assertEquals(name, "Leanne Graham");
+
+		String uname = response.jsonPath().getString("[0].username");
+		Assert.assertEquals(uname, "Bret");
+
+		String email = response.jsonPath().getString("[0].email");
+		Assert.assertEquals(email, "Sincere@april.biz");
+	}
 }
